334f2ac2f125a855ae525c624bc5c0d63705eb46
joyanedel <joyanede@alumnos.inf.utfsm.cl>
2025-04-10 23:38:11 -0400

    Fix shutdown bug

--- src/aio_actors/models.py
+++ src/aio_actors/models.py
@@ -12,7 +12,6 @@ class Actor(Generic[T], ABC):
 
     def __init__(self) -> None:
         self._queue = asyncio.Queue[T]()
-        self._lock = asyncio.Lock()
         self.__running_tasks: set[asyncio.Task[None]] = set()
 
         self._started = False
@@ -26,16 +25,14 @@ class Actor(Generic[T], ABC):
 
         self._started = True
         bg_task = asyncio.create_task(self.__background_event_loop())
+        bg_task.add_done_callback(self.__running_tasks.discard)
         self.__running_tasks.add(bg_task)
 
     async def send_message(self, message: T) -> None:
         """
         Sends a message to the actor.
         """
-
-        if not self._started:
-            raise RuntimeError("Actor is not started")
-
+        self.__verify_started()
         await self._queue.put(message)
 
     async def __background_event_loop(self):
@@ -53,13 +50,21 @@ class Actor(Generic[T], ABC):
         Shuts down the actor.
         Waits for all running tasks to complete.
         """
+
+        self.__verify_started()
+        await self._queue.put(None)
+
+        await asyncio.sleep(0)
+        # Wait for all running tasks to complete
+        await asyncio.wait(
+            self.__running_tasks | {asyncio.create_task(asyncio.sleep(0))},
+            return_when=asyncio.ALL_COMPLETED,
+        )
+        self._started = False
+
+    def __verify_started(self):
         if not self._started:
             raise RuntimeError("Actor is not started")
-
-        await self._queue.put(None)
-        await asyncio.wait(self.__running_tasks, return_when=asyncio.ALL_COMPLETED)
-
-        self._started = False
 
     @abstractmethod
     async def handle_message(self, message: T) -> None:
